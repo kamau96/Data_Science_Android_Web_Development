b54191edf2f526db59b8c87d56e63d49c610c46e
Kamau Wainaina <benson.kamau1738@gmail.com>
2024-11-20 13:30:09 +0300

    plot setup file created and revisiting code to ensure I understand time series started

---
+++ Data_Science_Development/Python_Programming/Future_sales/.ipynb_checkpoints/plot_setup-checkpoint.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use("seaborn-whitegrid")
+plt.rc(
+    "figure",
+    autolayout=True,
+    figsize=(11, 4),
+    titlesize=18,
+    titleweight='bold',
+)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=16,
+    titlepad=10,
+)
+#%config InlineBackend.figure_format = 'retina'
