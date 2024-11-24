3ff248c7c5d492f73369b1d50a28504c72eb30cd
Kamau Wainaina <benson.kamau1738@gmail.com>
2024-11-24 13:04:36 +0300

    prev lessons

--- Data_Science_Development/Python_Programming/Future_sales/helpful_functions.py
+++ Data_Science_Development/Python_Programming/Future_sales/helpful_functions.py
@@ -11,14 +11,14 @@ class MLFunctions():
         counter = 0
         
         # using a dictionary, each date is assigned a number.
-        for date in self.data.index:
+        for date in self.data['date']:
             if date not in date_dict:
                 date_dict[date] = counter 
                 counter += 1
             else:
                 pass
         # list with numeric values equal to date is created.
-        for date in self.data.index:
+        for date in self.data['date']:
             arr.append(date_dict[date])
         
         return arr
